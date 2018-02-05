97e0bcc388ef0437741999a3a4881a120e7f62ad
Max Neunhoeffer <max@arangodb.com>
2018-02-05 15:19:03 +0100

    Have a sensible example route.

--- foxx-demo/index.js
+++ foxx-demo/index.js
@@ -1,12 +1,18 @@
 "use strict";
 
-const router = require("@arangodb/foxx/router")();
+const router = require('@arangodb/foxx/router')();
 module.context.use(router);
 
-router.get((req, res) => {
-  res.send({ hello: "world" });
-});
+const db = require('@arangodb').db;
+const joi = require('joi');
 
-router.post((req, res) => {
-  res.send({ hello: "world" });
-});
+router.get((req, res) => {
+  const year = req.queryParams.year;
+  const members = db._query(`FOR c IN customers
+                               FILTER SUBSTRING(c.memberSince, 0, 4) == @year
+                               RETURN c`, { year }).toArray();
+  res.send(members);
+}).queryParam('year', joi.string().required(), 'Year of customers to return.')
+  .description('Returns the list of customers that signed up in the given year.')
+  .response(['application/json'], 'List of customer objects.');
+
--- makeShop.sh
+++ makeShop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -it --net=host -v `pwd`:/mnt arangodb arangosh --javascript.execute /mnt/shop.js
+docker run -it --net=host -v `pwd`:/mnt arangodb arangosh --javascript.execute /mnt/shop.js --server.endpoint tcp://ec2-35-167-245-247.us-west-2.compute.amazonaws.com:8529 
 
